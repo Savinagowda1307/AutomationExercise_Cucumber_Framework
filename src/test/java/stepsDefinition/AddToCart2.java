eab010955861295fabcb53fd4ee5dc5159107995
Savinagowda1307 <91127081+Savinagowda1307@users.noreply.github.com>
2025-02-19 18:35:54 +0530

    Update AddToCart2.java

--- src/test/java/stepsDefinition/AddToCart2.java
+++ src/test/java/stepsDefinition/AddToCart2.java
@@ -90,34 +90,3 @@ String pName;
 	}
 }
 
-/*@When("user add {string} to the cart")
-	public void user_add_to_the_cart(String pruductName) {
-		 hp=new Homepage(Basclass.getDriver());
-		 pp=new ProductPage(Basclass.getDriver());
-		 boolean found=false;
-		 pName=pruductName.replaceAll("[\\s-]", "");
-	   //System.out.println(p);
-	   for(WebElement bt:hp.viewCartBtns)
-	   {
-		   bt.click();
-		   String p2=pp.getProductName().replaceAll("[\\s-]", "");
-		   if(p2.equalsIgnoreCase(pName))
-		   {
-			   pp.clickOnAddtoCart();
-			   pp.clickOnviewCartBtn();
-			   found=true;
-			   System.out.println("found:"+pruductName);
-			   break;
-		   }
-		   else
-			   Basclass.getDriver().navigate().back();
-		   
-	   }
-	   if(found==false)
-	   {
-		   System.out.println("there is no product Named:"+pruductName);
-	   }
-	   
-	}
-
-*/
